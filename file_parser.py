fe4af1a86b50fd2359b9f2b45556989ce5f2a354
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-09-04 07:42:18 +0300

    Update file_parser.py

--- file_parser.py
+++ file_parser.py
@@ -3,6 +3,15 @@ File parser for knapsack problems
 """
 
 def fileparser(filename):
+    """ parses a knapsack brolem instance file.
+    Args:  
+        filename: A file
+    Returns:
+        numberofitems: the number of items
+        maxweight: the maximum weight of the knapsack
+        item: a list with the profit of every item
+        weight: a list with the weight of every item
+    """
     item=[]
     weight=[]
     with open(filename, 'r') as f:
